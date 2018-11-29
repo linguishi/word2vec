487ea0c109ccd0642297d5ed6690420a6d7eb998
lin <513228837@qq.com>
2018-11-30 00:42:45 +0800

    before upadate the variables

--- test.py
+++ test.py
@@ -75,7 +75,8 @@ if __name__ == '__main__':
     # 词数
     vocabulary_size = 100
 
-    data, count, dictionary, reverse_dictionary = build_dataset(data, vocabulary_size)
+    data, count, dictionary, reverse_dictionary = build_dataset(
+        data, vocabulary_size)
     print('Most common words (+UNK)', count[:5])
     print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
 
@@ -94,25 +95,29 @@ if __name__ == '__main__':
     skip_window = 1  # How many words to consider left and right.
     num_skips = 2  # How many times to reuse an input to generate a label.
     num_sampled = 64  # Number of negative examples to sample.
+    learning_rate = 1.0
+    train_steps = 100001
 
     # 验证相关的参数
     valid_size = 16  # Random set of words to evaluate similarity on.
     valid_window = 100  # Only pick dev samples in the head of the distribution.
-    valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+    valid_examples = np.random.choice(
+        valid_window, valid_size, replace=False)
 
     # 开始建图
     graph = tf.Graph()
     with graph.as_default():
         
         with tf.name_scope('inputs'):
-            train_input = tf.placeholder(tf.int32, shape=[batch_size])
-            train_label = tf.placeholder(tf.int32, shape=[batch_size])
+            train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+            train_labels = tf.placeholder(tf.int32, shape=[batch_size])
             valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
         with tf.name_scope('embeddings'):
             embeddings = tf.Variable(
-                tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-            emb_vec = tf.nn.embedding_lookup(embeddings, train_input)
+                tf.random_uniform(
+                    [vocabulary_size, embedding_size], -1.0, 1.0))
+            emb_vec = tf.nn.embedding_lookup(embeddings, train_inputs)
         
         with tf.name_scope('weights'):
             weights = tf.Variable(tf.truncated_normal(
@@ -121,3 +126,34 @@ if __name__ == '__main__':
 
         with tf.name_scope('biases'):
             biases = tf.Variable(tf.zeros([vocabulary_size]))
+        
+        with tf.name_scope('loss'):
+            loss = tf.reduce_mean(
+                tf.nn.nce_loss(
+                    weights=weights,
+                    biases=biases,
+                    labels=train_labels,
+                    inputs=emb_vec,
+                    num_sampled=num_sampled,
+                    num_classes=vocabulary_size))
+
+        tf.summary.scalar('loss', loss)
+
+        with tf.name_scope('optimizer'):
+            optimizer = tf.train.GradientDescentOptimizer(
+                learning_rate).minimize(loss)
+
+        init = tf.global_variables_initializer()
+
+    with tf.Session(graph=graph) as sess:
+        writer = tf.summary.FileWriter('log', sess.graph)
+        init.run()
+        print 'Finish Variables Initialization...'
+        averate_loss = 0
+        for step in xrange(train_steps):
+            batch_inputs, batch_labels = generate_batch(
+                batch_size, num_skips, skip_window)
+            feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+
+
+        
